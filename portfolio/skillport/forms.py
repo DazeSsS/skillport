f1f86cc1f769f2a8ac8ba5141e429914ec6adaf9
Daze <ivanrakov540@gmail.com>
2023-04-16 03:22:44 +0500

    fix signing up

--- portfolio/skillport/forms.py
+++ portfolio/skillport/forms.py
@@ -4,9 +4,18 @@ from django.forms import ModelForm
 from .models import Project
 
 class CreateUserForm(UserCreationForm):
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'login'
+        self.fields['email'].widget.attrs['class'] = 'email'
+        self.fields['password1'].widget.attrs['class'] = 'password'
+        self.fields['password2'].widget.attrs['class'] = 'password'
 
 
 class ProjectForm(ModelForm):
